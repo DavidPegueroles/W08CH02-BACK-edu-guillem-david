8bd254019a129234bdb9a4922188b65cb510334f
GuillemXanxo <jscodebaker@gmail.com>
2022-03-02 19:53:51 +0100

    Created showTuits controller

--- src/server/controllers/tuitahControllers.js
+++ src/server/controllers/tuitahControllers.js
@@ -1,3 +1,8 @@
-const showTuits = () => {};
+const Tuit = require("../../database/models/Tuit");
+
+const showTuits = async (req, res) => {
+  const tuits = await Tuit.find();
+  res.status(200).json({ tuits });
+};
 
 module.exports = { showTuits };
